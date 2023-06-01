a83bbac9701354fbf9e3daab9af2bb2798edc9eb
Feodor Kachinsky <fedor15zil@gmail.com>
2023-06-01 17:31:52 +0300

    Added package structure

---
+++ src/main/java/com/innowise/springemployeedataaccounting/SpringEmployeeDataAccountingApplication.java
@@ -0,0 +1,13 @@
+package com.innowise.springemployeedataaccounting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringEmployeeDataAccountingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringEmployeeDataAccountingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/innowise/springemployeedataaccounting/SpringEmployeeDataAccountingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.innowise.springemployeedataaccounting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringEmployeeDataAccountingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
